e0320b24f8d5c7087e8d207dcb4644e70e840e81
phitazero <kkeke8080@gmail.com>
2025-11-21 21:18:52 +0200

    added VersionList::fetch()

--- src/models/version_list.rs
+++ src/models/version_list.rs
@@ -1,10 +1,12 @@
 use crate::Modpack;
+use crate::requests;
 use serde::Deserialize;
 use std::ops::Deref;
+use std::process::exit;
 
 // dummy
 #[derive(Deserialize)]
-pub struct Version;
+pub struct Version {}
 
 #[derive(Deserialize)]
 pub struct VersionList(Vec<Version>);
@@ -14,5 +16,26 @@ impl Deref for VersionList {
 
 	fn deref(&self) -> &Self::Target {
 		&self.0
+	}
+}
+
+impl VersionList {
+	pub fn fetch(slug: &String, modpack: &Modpack) -> VersionList {
+		let url = format!("https://api.modrinth.com/v2/project/{}/version", slug);
+
+		let mut params: Vec<(&str, &str)> = Vec::new();
+
+		let loaders_str = format!("[\"{}\"]", modpack.loader);
+		params.push(("loaders", &loaders_str));
+
+		let versions_str = format!("[\"{}\"]", modpack.version);
+		params.push(("game_versions", &versions_str));
+
+		requests::sync_get(&url, params)
+			.unwrap_or_else(|err_msg| {
+				eprintln!("error: couldn't fetch versions for \'{slug}\'");
+				eprintln!("{err_msg}");
+				exit(1);
+			})
 	}
 }
