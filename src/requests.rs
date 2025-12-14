78842a9cf4ec8616210a1bfb86452bc5e237e85e
phitazero <kkeke8080@gmail.com>
2025-12-14 23:31:26 +0200

    added sync_download()

--- src/requests.rs
+++ src/requests.rs
@@ -2,6 +2,9 @@ use crate::utils;
 use reqwest::header::{self, HeaderMap, HeaderValue};
 use serde_urlencoded;
 use serde::de::DeserializeOwned;
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 
 const USER_AGENT: &str = "phitazero/modman";
 
@@ -42,4 +45,33 @@ where T: DeserializeOwned {
 		.map_err(|err|
 			format!("failed to parse JSON\n{}", utils::format_json_error(err))
 		)
+}
+
+pub fn sync_download<P>(path: P, url: &str) -> Result<(), String>
+where P: AsRef<Path> {
+	let path = path.as_ref();
+
+	let response = reqwest::blocking::get(url)
+		.map_err(|_| format!("GET request to {} failed", url))?;
+
+	let status = response.status();
+
+	if !status.is_success() {
+		return Err(format!(
+			"GET request to {} failed with status: {}",
+			url, status
+		));
+	}
+
+	let bytes = response
+		.bytes()
+		.map_err(|_| String::from("couldn't read download data"))?;
+
+	let mut file = File::create(path)
+		.map_err(|_| String::from("couldn't create file"))?;
+
+	file.write_all(&bytes)
+		.map_err(|_| String::from("couln't write to file"))?;
+
+	Ok(())
 }
