997a7955bd684f53c3203e25497ff5ca1e421731
phitazero <kkeke8080@gmail.com>
2025-12-17 00:13:13 +0200

    added Modpack::install_single()

--- src/models/modpack.rs
+++ src/models/modpack.rs
@@ -1,9 +1,9 @@
 use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::process::exit;
-use crate::utils;
+use crate::{utils, requests};
 use crate::MANIFEST_FILENAME;
-use crate::LocalMod;
+use crate::{LocalMod, Version, VersionFile};
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
@@ -65,5 +65,41 @@ impl Modpack {
 			eprintln!("error: modpack required");
 			exit(1);
 		})
+	}
+
+	pub fn install_single(&mut self, version: &Version, is_dep: bool) -> Result<(), String> {
+		eprintln!(
+			"Installing single \'{}\' : \'{}\'  (dependency={is_dep})",
+			version.slug,
+			version.version_number
+		);
+
+		let version_file = VersionFile::select_primary(&version.files)?;
+
+		let target_path = utils::current_dir()
+			.join("minecraft")
+			.join("mods")
+			.join(&version_file.filename);
+
+		requests::sync_download(target_path, &version_file.url)?;
+
+		let deps = if !is_dep {
+			version.dependencies.clone()
+		} else {
+			Vec::new()
+		};
+
+		let local_mod = LocalMod {
+			slug: version.slug.clone(),
+			project_id: version.project_id.clone(),
+			dependencies: deps,
+			is_dependency: is_dep,
+			file: version_file.filename.clone(),
+		};
+
+		self.mods.push(local_mod);
+		self.save();
+
+		Ok(())
 	}
 }
