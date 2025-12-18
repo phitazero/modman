b6880b93670720b109ae94c0de251c7015eaa4f1
phitazero <kkeke8080@gmail.com>
2025-12-18 20:29:36 +0200

    added batch version list fetching

--- src/models/version_list.rs
+++ src/models/version_list.rs
@@ -27,6 +27,15 @@ impl VersionList {
 		Ok(version_list)
 	}
 
+	pub fn batch_fetch(slugs: Vec<String>, modpack: &Modpack) -> BatchVersionListFetch<'_> {
+		BatchVersionListFetch {
+			n_slugs: slugs.len() as u8,
+			slugs_iter: slugs.into_iter(),
+			failed: Vec::new(),
+			modpack
+		}
+	}
+
 	pub fn is_empty(&self) -> bool {
 		self.0.is_empty()
 	}
@@ -34,5 +43,45 @@ impl VersionList {
 	pub fn latest(&self) -> Result<&Version, String> {
 		self.0.last()
 			.ok_or(String::from("no versions present"))
+	}
+}
+
+pub struct BatchVersionListFetch<'a> {
+	pub failed: Vec<String>,
+	modpack: &'a Modpack,
+	slugs_iter: std::vec::IntoIter<String>,
+	n_slugs: u8,
+}
+
+impl Iterator for BatchVersionListFetch<'_> {
+	type Item = (String, Result<VersionList, String>);
+
+	fn next(&mut self) -> Option<Self::Item> {
+		match self.slugs_iter.next() {
+			Some(slug) => {
+				let version_list = VersionList::fetch(&slug, self.modpack);
+
+				if version_list.is_err() {
+					self.failed.push(slug.clone());
+				}
+
+				Some((slug, version_list))
+			},
+			// after iterating over all slugs
+			None => {
+				let n_failed = self.failed.len() as u8;
+
+				if n_failed > 0 {
+					eprintln!("warning: {n_failed} request(s) failed for slugs:");
+					eprintln!("{}", self.failed.join(", "));
+				}
+
+				if n_failed == self.n_slugs {
+					eprintln!("warning: all requests failed");
+				}
+
+				None
+			}
+		}
 	}
 }
