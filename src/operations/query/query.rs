efa2ec4859b766ea04e69242b6b92da675ba3808
phitazero <kkeke8080@gmail.com>
2025-12-21 15:38:26 +0200

    added q and e options

--- src/operations/query/query.rs
+++ src/operations/query/query.rs
@@ -2,15 +2,21 @@ use crate::{Modpack};
 use crate::arg_parser::ParsedArgs;
 
 pub fn command_query(parsed_args: ParsedArgs) {
-	parsed_args.check_validity(&['Q']);
+	parsed_args.check_validity(&['Q', 'd', 'e']);
 
-	let args = parsed_args.args;
+	let args = &parsed_args.args;
 
 	let modpack = Modpack::require_current();
 	let mut mods = modpack.mods;
 
 	if args.len() != 0 {
 		mods.retain(|m| args.contains(&m.slug));
+	}
+
+	if parsed_args.option('d') && !parsed_args.option('e') {
+		mods.retain(|m| m.is_dependency);
+	} else if !parsed_args.option('d') && parsed_args.option('e') {
+		mods.retain(|m| !m.is_dependency);
 	}
 
 	for local_mod in mods {
