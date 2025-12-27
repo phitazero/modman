ed959dc2fff437b4fa497de8ed504af164c0a1f2
phitazero <kkeke8080@gmail.com>
2025-12-27 18:12:44 +0200

    added -Mi operation

---
+++ src/operations/modpack/info.rs
@@ -0,0 +1,28 @@
+use crate::arg_parser::ParsedArgs;
+use crate::Modpack;
+use crate::utils;
+
+pub fn command_info(parsed_args: ParsedArgs) {
+	parsed_args.check_validity(&['M', 'i']);
+
+	let modpack = Modpack::require_current();
+
+	/*
+	Path.file_name() returns None in two cases:
+	1) It's root directory.
+	If code execution reached this point, Modpack::require_current()
+	hasn't exited, which means that we're in a valid modpack, which can't
+	be created in root dir. Alternatively it's possible with modpack
+	manifest manually created in root dir, but i'm not an idiot to do that
+	2) Path terminates in ..
+	env::current_dir() can't return a path terminating in ..
+
+	Hence .file_name() will always be a Some and .unwrap is safe.
+	*/
+	let path = utils::current_dir();
+	let name = path.file_name().unwrap();
+
+	println!("========== {} ==========", name.display());
+	println!("Version: {}", modpack.version);
+	println!("Loader: {}", modpack.loader);
+}
--- src/operations/modpack/mod.rs
+++ src/operations/modpack/mod.rs
@@ -2,11 +2,14 @@ use crate::arg_parser::ParsedArgs;
 use super::dispatcher;
 
 mod modpack;
+mod info;
 
 pub fn dispatch(parsed_args: ParsedArgs) {
 	dispatcher::dispatch(
 		parsed_args,
 		modpack::command_modpack,
-		&[]
+		&[
+			('i', info::command_info),
+		]
 	);
 }
